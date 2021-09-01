7c2f40ae77b01de7396be361b955149df5d92081
Хасанов Михаил <mkhasanov@lan.lanit.ru>
2021-09-01 18:36:57 +0300

    qwe

--- Vvod.py
+++ Vvod.py
@@ -4,3 +4,13 @@ w=int(input())#прочитать строку с клавиатуры и пре
 print(q)
 print(w)
 print(1+3)
+
+name=input('your name')
+print('hello ',name)
+
+a=int(input())
+print(a**3)
+
+a=int(input())
+b=int(input())
+print(a+b/2)
