053a34a23ba615325d3dc7cbd3e0c3ecb1aa8b5c
Хасанов Михаил <mkhasanov@lan.lanit.ru>
2021-09-01 14:30:26 +0300

    hello

---
+++ Test.py
@@ -0,0 +1,30 @@
+print('Hello LANIT')
+print(5*3+25-3/2)
+print(11111*1111111)
+print(11111*1111111)
+a=38
+b=5
+print(a/b)
+print(a//b)#целочисленное деление
+print(a%b)#получение отсатка
+#print(42/(4+2*(-2)))#ответ ZeroDivisionError: division by zero
+print(2014**14)
+print(-12)
+print(+12)
+#print(-*12)#SyntaxError: invalid syntax
+print(-+-+12)
+print(+-+12)
+print(1/3)
+print(5e-1)
+print(12345e-3)
+print(12345e3)
+print(1.2345e3)
+print(1234*5)
+print(1.2345e-3)
+print(2014.0**14)
+print(7/3)
+print(7//3)
+int(2.99)
+print(int(-1.6))
+print(9**19 - int(float(9**19)))
+print(type(12))#ответ <class 'int'>
